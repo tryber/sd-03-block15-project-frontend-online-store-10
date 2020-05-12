345921dcb6583cb4c8fa27c534126730756be364
Rhian Castro <rhi.castro@gmail.com>
2020-05-12 17:07:47 -0300

    lol

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export const getCategories = async () => {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
-  return fetch(url);
+  return fetch(url).then((data) => data.json());
 };
 
 export const getProductsFromCategoryAndQuery = async (categoryId, query) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  return fetch(url);
+  return fetch(url).then((data) => data.json());
 };
