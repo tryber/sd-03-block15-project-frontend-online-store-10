b7fbbd26645397033e3023b412a1d4bbbda20c15
Rhian Castro <rhi.castro@gmail.com>
2020-05-12 16:57:06 -0300

    code climate const fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export const getCategories = async () => {
-  let url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   return fetch(url);
 };
 
 export const getProductsFromCategoryAndQuery = async (categoryId, query) => {
-  let url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   return fetch(url);
 };
