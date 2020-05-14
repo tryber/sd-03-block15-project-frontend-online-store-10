3b078dfe8c8fe2564e819b59c8f6e3dbc0a5c5c2
Rhian Castro <rhi.castro@gmail.com>
2020-05-14 16:59:04 -0300

    added navbar component and other product details

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Media } from 'react-bootstrap';
+import Navbar from '../components/NavBar';
 
 class ProductDetail extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class ProductDetail extends React.Component {
     const { product } = this.props.location.state;
     return (
       <div>
+        <Navbar />
         <h4>{product.title}</h4>
         <ul className="list-unstyled">
           <Media as="li">
@@ -18,9 +20,12 @@ class ProductDetail extends React.Component {
               alt="Generic placeholder"
             />
             <Media.Body>
-              <h5>{product.attributes[0].value_name}</h5>
-              <p>{product.attributes[2].value_name}</p>
+              <h5>Marca: {product.attributes[0].value_name}</h5>
+              <p>Modelo:{product.attributes[2].value_name}</p>
               <p>Aceita Mercado Pago: {product.accepts_mercadopago ? 'Sim' : 'Não'}</p>
+              <p>
+                Preço: {product.currency_id}:{product.price}
+              </p>
             </Media.Body>
           </Media>
         </ul>
