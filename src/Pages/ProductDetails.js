e33f3fd29eaa09b88c8d2451f24fd478b862e1d9
William Igor <williamigorsilva@gmail.com>
2020-05-17 16:47:39 -0300

    Remove issues CC

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -4,9 +4,9 @@ import Card from 'react-bootstrap/Card';
 import NavBar from '../components/NavBar';
 
 class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
@@ -15,14 +15,14 @@ class ProductDetails extends React.Component {
         <h2>Detalhes do produto </h2>
         <Card className="">
           <h3 data-testid="product-detail-name">nome do produto</h3>
-          <img />
+          {/* <img /> */}
           <p>descrição</p>
           <p>preço</p>
         </Card>
         <div>
           <button type="button">Adicionar ao carrinho</button>
         </div>
-        <Link to="">Página inicial</Link>
+        <Link to="/">Página inicial</Link>
       </div>
     );
   }
