12ea8882242d52fe29753744db4c9cf93a617be7
William Igor <williamigorsilva@gmail.com>
2020-05-17 21:59:41 -0300

    item 3

--- src/components/InitialCartMessage.js
+++ src/components/InitialCartMessage.js
@@ -4,7 +4,7 @@ class InitialCartMessage extends React.Component {
   render() {
     return (
       <div>
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+        {/* <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1> */}
       </div>
     );
   }
