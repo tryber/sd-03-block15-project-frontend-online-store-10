480aa391354cef14ffb2742f651434d501878d22
William Igor <williamigorsilva@gmail.com>
2020-05-18 16:00:58 -0300

    issues cc

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import CardDeck from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 import NavBar from '../components/NavBar';
 import Rating from '../components/Rating';
 import * as cartActions from '../actions/cart';
@@ -24,29 +24,6 @@ class ProductDetails extends React.Component {
     return (
       <div className="text-center">
         <NavBar />
-        <CardDeck className="productDetails">
-          <h2 className="">Detalhes do produto </h2>
-          <div>
-            <img
-              className="productDetails"
-              src={product.thumbnail}
-              alt="foto produto"
-            />
-          </div>
-          <h3 data-testid="product-detail-name">{product.title}</h3>
-          <p>descrição</p>
-          Bagatela de
-          <p>{product.currency_id}: {product.price}</p>
-          <div>
-            <Button
-              data-testid="product-detail-add-to-cart"
-              type="button"
-            >
-              Adicionar ao carrinho
-          </Button>
-          </div>
-          <Link to="/" style={{ textDecoration: 'none' }}>Página inicial</Link>
-        </CardDeck>
         <h2 className="text-center">Detalhes do produto </h2>
         <div>
           <img className="productDetails" src={product.thumbnail} alt="foto produto" />
@@ -60,12 +37,12 @@ class ProductDetails extends React.Component {
           </p>
         </Card>
         <div>
-          <button data-testid="product-detail-add-to-cart" type="button" onClick={this.addNewItem}>
+          <Button data-testid="product-detail-add-to-cart" type="button" onClick={this.addNewItem}>
             Adicionar ao carrinho
-          </button>
+          </Button>
           <Rating />
         </div>
-        <Link to="/">Página inicial</Link>
+        <Link to="/" style={{ textDecoration: 'none' }}>Página inicial</Link>
       </div>
     );
   }
