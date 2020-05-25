9ca602a5afe6df340f74f969780dab147c602f90
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-25 01:00:15 -0300

    test

--- src/controllers/detailProductControler.js
+++ src/controllers/detailProductControler.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class DetailProduct extends React.Component {
   render() {
     const product = this.props.location.state.product;
-    return <div >
-      <h1 data-testid="product-detail-name">{product.title}</h1>
-    </div>
+    return (
+      <div>
+        <h1 data-testid="product-detail-name">{product.title}</h1>
+      </div>
+    );
   }
 }
 
--- src/views/SearchBarHome.js
+++ src/views/SearchBarHome.js
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom';
 
 class SearchBarHome extends React.Component {
   render() {
-    const {
-      searchText, onSearchTextChange, onSearchtTextClick, 
-    } = this.props;
+    const { searchText, onSearchTextChange, onSearchtTextClick } = this.props;
     return (
       <form>
         <label htmlFor="IncluiOTexto">
           Inclui o texto:
-          <input type="text" data-testid="query-input" value={searchText} onChange={onSearchTextChange} />
-          <input type="button" data-testid="query-button" value="ok" onClick={onSearchtTextClick} />
+          <input
+            type="text"
+            data-testid="query-input"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
+          <input
+            type="button"
+            data-testid="query-button"
+            value="ok"
+            onClick={onSearchtTextClick}
+          />
           <Link data-testid="shopping-cart-button" to="/Cart">carrinho</Link>
         </label>
       </form>
