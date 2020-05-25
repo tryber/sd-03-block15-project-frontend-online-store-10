3f3f16c36df26ad5210d2d1ff7947b6037be9e97
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-25 00:16:08 -0300

    test

--- src/views/CategoriesHome.js
+++ src/views/CategoriesHome.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import CardCategories from './CardCategories';
 
 class CategoriesHome extends React.Component {
--- src/views/ItensListHome.js
+++ src/views/ItensListHome.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import CardItemHome from './CardItemHome';
 
 class ItensListHome extends React.Component {
