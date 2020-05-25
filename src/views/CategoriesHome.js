c795d7bfa4bec7352c3a5c5336a55972ecbfb6ec
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-25 00:26:25 -0300

    test

--- src/views/CategoriesHome.js
+++ src/views/CategoriesHome.js
@@ -5,9 +5,13 @@ class CategoriesHome extends React.Component {
   render() {
     return (
       <div className="categories">
-        {this.props.categories.map
-        ((item) => <CardCategories key={item.id} id={item.id} up={this.props.update}
-          categorie={item.name}/>)}
+        {this.props.categories.map((item) =>
+          <CardCategories
+          key={item.id}
+          id={item.id}
+          up={this.props.update}
+          categorie={item.name}
+          />)}
       </div>
     );
   }
