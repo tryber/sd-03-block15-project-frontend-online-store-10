ca9cc1fd9fa1d500820b02a526e63a1128b96493
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-25 01:31:11 -0300

    test

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -26,24 +26,26 @@ class Home extends React.Component {
   };
 
   up(is) {
-    api.getProductsFromCategoryAndQuery(is,false).then(query => this.setState({itensList: query.results}));
+    api.getProductsFromCategoryAndQuery(is,false).then(query =>
+      this.setState({itensList: query.results}));
   };
 
   onSearchtTextClick() {
-    api.getProductsFromCategoryAndQuery(false,this.state.searchText).then(query => this.setState({itensList: query.results}));
+    api.getProductsFromCategoryAndQuery(false,this.state.searchText).then((query) =>
+      this.setState({ itensList: query.results }));
   };
 
   render() {
     const { match: { params: { Id } } } = this.props;
     console.log(Id);
-    return(
+    return (
       <div className="ItensListHome">
-      <CategoriesHome categories={this.state.categories} update={this.up} />
-      <SearchBarHome searchText={this.state.searchText}
-       onSearchTextChange={this.onSearchTextChange}
-       onSearchtTextClick={this.onSearchtTextClick} />
-      <ItensListHome searchText={this.state.searchText} itensList={this.state.itensList}   /> 
-      </div>   
+        <CategoriesHome categories={this.state.categories} update={this.up} />
+        <SearchBarHome searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          onSearchtTextClick={this.onSearchtTextClick} />
+        <ItensListHome searchText={this.state.searchText} itensList={this.state.itensList} />
+      </div>
     );
   }
 }
