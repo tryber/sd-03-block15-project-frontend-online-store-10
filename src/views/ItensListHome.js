ba3b7d1394bb75adf175c3c54b90c9251730d473
felipedoss <felipecaetano25.1996@gmail.com>
2020-05-25 00:38:49 -0300

    test

--- src/views/ItensListHome.js
+++ src/views/ItensListHome.js
@@ -3,11 +3,20 @@ import CardItemHome from './CardItemHome';
 
 class ItensListHome extends React.Component {
   render() {
-    if (!this.props.itensList.length)
-      return <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</div> 
-   return (<div>
-     {this.props.itensList.map(item => <CardItemHome key={item.id} item={item}/>)}
-   </div> );
+    if (!this.props.itensList.length) {
+      return <div data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </div>;
+    }
+    return (
+      <div>
+      {this.props.itensList.map((item) =>
+        <CardItemHome
+          key={item.id}
+          item={item}/>
+        )}
+      </div>
+    );
   }
 }
 
